aa6d3e7047635d451d70806d2b52b3fa7a3e65d6
danny <step.kozbvb@gmail.com>
2020-03-06 18:57:16 +0100

    init

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
     setuptools.setup(
-        name="trello_client-basics-api-username", version="0.0.1", author="STEPAN KOZURAK",
+        name="trello_client-dannycrief", version="0.0.1", author="STEPAN KOZURAK",
         author_email="step.kozbvb@icloud.com",
         description="First console client for Trello!", long_description=long_description,
         long_description_content_type="text/markdown",
-        url="[https://github.com/basics-api-username/trello_client](https://github.com/basics-api-username/test_client)"
+        url="[https://github.com/dannycrief/trello-console-client]"
     )
-# Адрес сайта вашего пакета, можно указать ссылку на репозиторий GitHub. packages=setuptools.find_packages(),
-# classifiers=[ "Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System ::
-# OS Independent", ], python_requires='>=3.6',)
